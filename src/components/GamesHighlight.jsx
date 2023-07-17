6b175a480faa7a26a893c9c165c5de7435ee1e5c
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-07-17 15:11:08 -0300

    fix: GamesHIghLight com link

--- src/components/GamesHighlight.jsx
+++ src/components/GamesHighlight.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { styled } from "styled-components";
 import Jogos from "../ArrayGames/games";
+import { useNavigate } from "react-router-dom";
 
 function GamesHighlight() {
   const [index, setIndex] = useState(0);
   const jogoAtual = Jogos[index];
-
+  const navigate = useNavigate()
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % Jogos.length);
@@ -15,7 +16,7 @@ function GamesHighlight() {
   }, []);
 
   return (
-    <ComponenteDiv oncli>
+    <ComponenteDiv onClick={() => navigate(`/Game/${jogoAtual.title}`)}>
       <img src={jogoAtual.image} alt={jogoAtual.title} />
     </ComponenteDiv>
   );
